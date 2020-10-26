367aecedfdfd0a08adec5ddbec8f6dab6f4a6f89
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-10-26 23:40:49 +0100

    Build appbar

--- lib/models/screens/home_screen.dart
+++ lib/models/screens/home_screen.dart
@@ -1,8 +1,42 @@
+import 'package:e_commerce/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/back.svg"),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/search.svg",
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/cart.svg",
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        SizedBox(
+          width: kDefaultPaddin / 2,
+        ),
+      ],
+    );
   }
 }
