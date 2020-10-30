6195b4f638e20a899a3fef9ecfefe36076eb3caf
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-10-30 23:54:53 +0100

    Body background

--- lib/models/screens/details/components/body.dart
+++ lib/models/screens/details/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/constants.dart';
 import 'package:e_commerce/models/Product.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,49 @@ class Body extends StatelessWidget {
   const Body({Key key, this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: size.height * 0.3),
+                  height: 500,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24),
+                        topRight: Radius.circular(24),
+                      )),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Aristocratic Hand Bag",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        product.title,
+                        style: Theme.of(context).textTheme.headline4.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
