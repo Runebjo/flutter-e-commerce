728f4411c9e0a47c0bb2ef726aa295aee72ba8fd
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-11-01 22:37:25 +0100

    Added some colors

--- lib/models/screens/details/components/body.dart
+++ lib/models/screens/details/components/body.dart
@@ -19,6 +19,11 @@ class Body extends StatelessWidget {
               children: [
                 Container(
                   margin: EdgeInsets.only(top: size.height * 0.4),
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.16,
+                    left: kDefaultPaddin,
+                    right: kDefaultPaddin,
+                  ),
                   height: 500,
                   decoration: BoxDecoration(
                     color: Colors.white,
@@ -27,12 +32,71 @@ class Body extends StatelessWidget {
                       topRight: Radius.circular(24),
                     ),
                   ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Color"),
+                              Row(
+                                children: [
+                                  ColorDot(
+                                    color: Color(0xFF356C95),
+                                    isSelected: true,
+                                  ),
+                                  ColorDot(color: Color(0xFFF8C078)),
+                                  ColorDot(color: Color(0xFFA29B9B)),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
                 ProductTitleWithImage(product: product)
               ],
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class ColorDot extends StatelessWidget {
+  final Color color;
+  final bool isSelected;
+  const ColorDot({
+    Key key,
+    this.color,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: kDefaultPaddin / 4,
+        right: kDefaultPaddin / 2,
+      ),
+      padding: EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected ? color : Colors.transparent,
+        ),
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+        ),
       ),
     );
   }
