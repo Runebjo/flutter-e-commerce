ba1000669c8f6142f5058f7f3850b40043c592c8
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-11-02 20:59:12 +0100

    Added color and size

--- lib/models/screens/details/components/body.dart
+++ lib/models/screens/details/components/body.dart
@@ -2,6 +2,7 @@ import 'package:e_commerce/constants.dart';
 import 'package:e_commerce/models/Product.dart';
 import 'package:flutter/material.dart';
 
+import 'color_and_size.dart';
 import 'product_title_with_image.dart';
 
 class Body extends StatelessWidget {
@@ -32,71 +33,13 @@ class Body extends StatelessWidget {
                       topRight: Radius.circular(24),
                     ),
                   ),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Color"),
-                              Row(
-                                children: [
-                                  ColorDot(
-                                    color: Color(0xFF356C95),
-                                    isSelected: true,
-                                  ),
-                                  ColorDot(color: Color(0xFFF8C078)),
-                                  ColorDot(color: Color(0xFFA29B9B)),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+                  child: ColorAndSize(product: product),
                 ),
                 ProductTitleWithImage(product: product)
               ],
             ),
           )
         ],
-      ),
-    );
-  }
-}
-
-class ColorDot extends StatelessWidget {
-  final Color color;
-  final bool isSelected;
-  const ColorDot({
-    Key key,
-    this.color,
-    this.isSelected = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        top: kDefaultPaddin / 4,
-        right: kDefaultPaddin / 2,
-      ),
-      padding: EdgeInsets.all(2.5),
-      height: 24,
-      width: 24,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: isSelected ? color : Colors.transparent,
-        ),
-      ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: color,
-          shape: BoxShape.circle,
-        ),
       ),
     );
   }
