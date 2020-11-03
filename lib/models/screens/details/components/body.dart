c775bd25139b17bd515fce77a1e0fe83c7ebe91c
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-11-03 22:09:04 +0100

    Add description

--- lib/models/screens/details/components/body.dart
+++ lib/models/screens/details/components/body.dart
@@ -3,6 +3,7 @@ import 'package:e_commerce/models/Product.dart';
 import 'package:flutter/material.dart';
 
 import 'color_and_size.dart';
+import 'description.dart';
 import 'product_title_with_image.dart';
 
 class Body extends StatelessWidget {
@@ -19,27 +20,71 @@ class Body extends StatelessWidget {
             child: Stack(
               children: [
                 Container(
-                  margin: EdgeInsets.only(top: size.height * 0.4),
-                  padding: EdgeInsets.only(
-                    top: size.height * 0.16,
-                    left: kDefaultPaddin,
-                    right: kDefaultPaddin,
-                  ),
-                  height: 500,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(24),
-                      topRight: Radius.circular(24),
+                    margin: EdgeInsets.only(top: size.height * 0.35),
+                    padding: EdgeInsets.only(
+                      top: size.height * 0.12,
+                      left: kDefaultPaddin,
+                      right: kDefaultPaddin,
                     ),
-                  ),
-                  child: ColorAndSize(product: product),
-                ),
+                    height: 500,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24),
+                        topRight: Radius.circular(24),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        ColorAndSize(product: product),
+                        Description(product: product),
+                        CartCounter()
+                      ],
+                    )),
                 ProductTitleWithImage(product: product)
               ],
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class CartCounter extends StatefulWidget {
+  @override
+  _CartCounterState createState() => _CartCounterState();
+}
+
+class _CartCounterState extends State<CartCounter> {
+  int numOfItems = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        buildOutlineButton(),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: kDefaultPaddin / 2,
+          ),
+          child: Text(
+            "01",
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        )
+      ],
+    );
+  }
+
+  SizedBox buildOutlineButton() {
+    return SizedBox(
+      width: 40,
+      height: 32,
+      child: OutlineButton(
+        padding: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(13)),
+        onPressed: () {},
+        child: Icon(Icons.remove),
       ),
     );
   }
